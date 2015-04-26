766e3df9b77f8983519061051873397285cef783
ZiyiChen <chen216@mail.chapman.edu>
2015-04-26 00:19:09 +0000

    testing

--- app/controllers/rekt_controller.rb
+++ app/controllers/rekt_controller.rb
@@ -1,4 +1,6 @@
 class RektController < ApplicationController
   def index
+    result = Net::HTTP.get(URI.parse('https://na.api.pvp.net/api/lol/na/v1.2/champion?api_key=fc908f24-2c88-4ed9-80a6-d072ada9ed05'))
+    @hash = JSON.parse result
   end
 end
