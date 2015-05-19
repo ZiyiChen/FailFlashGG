53159befeec36dc37edc3337bbd09ac02e5ac4ac
ZiyiChen <chen216@mail.chapman.edu>
2015-05-19 03:25:10 +0000

    heroku debug 2

--- app/controllers/rekt_controller.rb
+++ app/controllers/rekt_controller.rb
@@ -8,6 +8,7 @@ class RektController < ApplicationController
   
   def get_summoner_by_name (name)
     result = Net::HTTP.get(URI.parse('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'+name+'?api_key=fc908f24-2c88-4ed9-80a6-d072ada9ed05'))
+    puts result
     @summoner = JSON.parse result
   end
   
